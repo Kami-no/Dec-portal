a7fa290255558e2bb838f21fd1dc5b665df17c40
Kami-no <admin@kami-no.ru>
2013-12-04 15:04:53 +1100

    Optimized authorization logic

--- auth-db.php
+++ auth-db.php
@@ -19,46 +19,45 @@ include_once ('config-db.php');
 
 // Connect to DB
 $db = new mysqli($db_host, $db_user, $db_pass, $db_base);
-if(mysqli_connect_errno()) {
+if (mysqli_connect_errno()) {
     $msg = "MySQL connection failed: ".mysqli_connect_error();
     setcookie('err',$msg);
     header('Location: index.php');
     exit;
 }
 
-// File-requests
-if(isset($_GET['get_file']) && isset($_SESSION['user_id'])) {
-    include('download.php');
-    mysqli_close($db);
-    exit;
-}
-
-// User-list requests
-if(isset($_GET['user_list']) && isset($_SESSION['admin'])) {
-    include('page-admins-list.php');
-    mysqli_close($db);
-    exit;
-}
-
-// Edit user
-if(isset($_GET['id']) && isset($_SESSION['admin'])) {
-    include('page-admins-usermod.php');
-    mysqli_close($db);
-    exit;
-}
-
-// Authorized admins
-if (isset($_SESSION['admin'])) {
-    include('page-admins.php');
-    mysqli_close($db);
-    exit;
-}
-
-// Authorized users
+// Authorized user
 if (isset($_SESSION['user_id'])) {
-    include('page-users.php');
-    mysqli_close($db);
-    exit;
+    // Get file for authorized user
+    if (isset($_GET['get_file'])) {
+        include('download.php');
+        mysqli_close($db);
+        exit;
+    }
+    // For admins
+    if ($_SESSION['admin'] == 1) {
+        // List users
+        if (isset($_GET['user_list'])) {
+            include('page-admins-list.php');
+            mysqli_close($db);
+            exit;
+        }
+        // Edit user
+        if (isset($_GET['id'])) {
+            include('page-admins-usermod.php');
+            mysqli_close($db);
+            exit;
+        }
+        // Admin's home
+        include('page-admins.php');
+        mysqli_close($db);
+        exit;
+    } else {
+        // Go to user's home
+        include('page-users.php');
+        mysqli_close($db);
+        exit;
+    }
 }
 
 // Authorize user
@@ -67,28 +66,17 @@ if (isset($_POST['login']) && isset($_POST['password'])) {
     $result = mysqli_query($db,$request);
     $done = mysqli_fetch_all($result,MYSQLI_ASSOC);
     $users = mysqli_num_rows($result);
-    //mysqli_free_result($result);
+    mysqli_free_result($result);
 
     // User authorization
     if ($users == 1) {
         $_SESSION['user_id'] = $done['0']['id'];
+        $_SESSION['admin'] = $done['0']['admin'];
     } else {
-        mysqli_close($db);
         setcookie('err','Неправильный логин или пароль.');
-        header('Location: index.php');
-        exit;
     }
 
-    // Authorized admins go to admins' page and others go to user's page
-    // TODO: verify setting SESSION equal $done
-    if ($done['0']['admin'] == 1) {
-        $_SESSION['admin'] = 1;
-        include('page-admins.php');
-        mysqli_close($db);
-        exit;
-    } else {
-        include('page-users.php');
-        mysqli_close($db);
-        exit;
-    }
+    mysqli_close($db);
+    header('Location: index.php');
+    exit;
 }
